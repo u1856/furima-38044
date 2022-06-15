fc806e9a4f714d6d3b3824f274c4212ac92a56b3
u1856 <sglspy856@gmail.com>
2022-06-15 21:24:33 +0900

    contentは存在しないカラムのため削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:content, :image, :name, :text, :price, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :price, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 
 end
