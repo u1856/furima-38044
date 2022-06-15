137a5591a88e4a30a5ab84983bed2309aa46abe9
u1856 <sglspy856@gmail.com>
2022-06-15 15:54:37 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,19 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :name, :text, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :prefectures_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
 end
